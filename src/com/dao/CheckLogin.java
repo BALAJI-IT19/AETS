b07a0a1c7c156f0e75284fb0b8942cc758592af8
Naveen A T <naveen.at@idp.com>
2022-10-20 16:57:34 +0530

    sysout

--- src/com/dao/CheckLogin.java
+++ src/com/dao/CheckLogin.java
@@ -15,18 +15,14 @@ public class CheckLogin {
 		ResultSet rs = null;
 		try {
 			con = DBConnection.connect();
-			System.out.print("\n\n\n"+email+"\n\n\n"+pwd+"\n\n\n");
 			String query = "select * from aets_emp_details where email=?";
 			PreparedStatement ps = con.prepareStatement(query);
-			System.out.println(query);
 			ps.setString(1, email);
-			System.out.println("\n\n\n");
 			rs = ps.executeQuery();
 			boolean flag = false;
 			while (rs.next()) {
 				flag = true;
 				String str = rs.getString(6);
-				System.out.print(str+"\n\n\n");
 				if (pwd.equals(str)) {
 					result = "success";
 				} else {
